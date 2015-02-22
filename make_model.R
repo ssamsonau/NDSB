c073998725ac0a58197d31435c10f436a28544e3
ssamsonau <ssamsonau@gc.cuny.edu>
2015-02-21 20:29:23 -0500

    make only one model. Do not do two step prediction.

--- make_model.R
+++ make_model.R
@@ -1,4 +1,5 @@
 # load prepared data
+#-------------------------------------------------
 library(data.table)
 imgTrainDT <- fread(unzip("imgTrainDT.zip"))
 #imgTrainDT <- fread("imgFeaturesTrainDT.csv")
@@ -7,6 +8,7 @@ pathCol <- imgTrainDT$path
 imgTrainDT[, path:=NULL]
 
 #preprocess with caret
+#--------------------------------------------------
 library(caret)
 nzv <- nearZeroVar(imgTrainDT)
 imgTrainDT[, eval(nzv):=NULL]
@@ -18,18 +20,18 @@ imgTrainDT[, eval(col.to.scale):=predict(preProcValues, imgTrainDT[, .SD, .SDcol
 
 #descrCor <- cor(imgTrainDT)
 #highlyCorDescr <- findCorrelation(descrCor, cutoff = .8)
-#imgTrainDT[, highlyCorDescr:=NULL]
+#imgTrainDT[, eval(highlyCorDescr):=NULL]
 
 
 ### SUBSET data for small computer
 subsetSize = -1 
-#-------------------------
+#--------------------------------------------------
 
 imgTrainDT[, output:=
              sapply( strsplit( pathCol, "&"), "[", 1) ]
 
-imgTrainDT[, genType:=as.factor(
-  sapply( strsplit(as.character(imgTrainDT$output), "_") , "[", 1))]
+#imgTrainDT[, genType:=as.factor(
+#  sapply( strsplit(as.character(imgTrainDT$output), "_") , "[", 1))]
 
 imgTrainDT[, output:=factor(output)]
 
@@ -48,54 +50,11 @@ print(str(trainDT.h2o[, 1:10]))
 Ncols <- ncol(trainDT.h2o)
 print(str(trainDT.h2o[, (Ncols-10):Ncols]))
 
-#takes too much time and uses only one core
-#random <- h2o.runif(trainDT.h2o, seed = 123456789)
-#train_hex <- h2o.assign(trainDT.h2o[random < .8,], "train_hex")
-#valid_hex <- h2o.assign(trainDT.h2o[random >= .8 & random < .9,], "valid_hex")
-#test_hex  <- h2o.assign(trainDT.h2o[random >= .9,], "test_hex")
-
+##-------------------------------------------------------
+#"train a model for output"
 train_hex_split <- h2o.splitFrame(trainDT.h2o, ratios = 0.8, shuffle = TRUE)
 
-"train a model for general type"
-
-#http://0xdata.com/docs/master/model/deep-learning/
-## Train a 50-node, three-hidden-layer Deep Neural Networks for 100 epochs
-grid_search_genType <- h2o.deeplearning(x = grep("V", names(trainDT.h2o), value=T),
-                                y = "genType",
-                                data = train_hex_split[[1]],
-                                validation = train_hex_split[[2]],
-                                #nfolds = 5,
-                                
-                                hidden=list(c(40, 40)),
-                                epochs = 100,
-                                activation=c("Rectifier"),
-                                classification = TRUE,
-                                balance_classes = FALSE, 
-                                adaptive_rate = TRUE,
-                                rho = c(0.92, 0.98),
-                                epsilon= c(1e-8, 1e-6),
-                                #l2=c(1e-5, 1e-4),
-                                #l1=c(0, 1e-5),
-                                fast_mode=TRUE)
-
-
-best_model_genType <- grid_search_genType@model[[1]]
-best_params_genType <- best_model_genType@model$params
-
-h2o.saveModel(object=best_model_genType, dir=".", name="best_model_genType.Rdata", force=T)
-
-print(best_model_genType)
-#print(best_params_genType)
-
-#print("error on test set _genType")
-#predicted_genType <- h2o.predict(best_model_genType, test_hex)
-#h2o.confusionMatrix(predicted_genType$predict, test_hex$genType)["Totals", "Error"]
-
-###------------------------------------------
-"train a model for output"
-
-## Train a 50-node, three-hidden-layer Deep Neural Networks for 100 epochs
-grid_search <- h2o.deeplearning(x = c( grep("V", names(trainDT.h2o), value=T), "genType"),
+grid_search <- h2o.deeplearning(x = c( grep("V", names(trainDT.h2o), value=T)),
                                 y = "output",
                                 data = train_hex_split[[1]],
                                 validation = train_hex_split[[2]],
@@ -117,15 +76,11 @@ grid_search <- h2o.deeplearning(x = c( grep("V", names(trainDT.h2o), value=T), "
 best_model <- grid_search@model[[1]]
 best_params <- best_model@model$params
 
-
 h2o.saveModel(best_model, dir=".", name="best_model.Rdata", force = T)
 
 print(best_model)
 
-
 #make prediction of output
-valDT <- data.table( as.matrix(train_hex_split[[2]]) )
-
 predicted_output <- h2o.predict(best_model, train_hex_split[[2]])
 #predicted_output$predict
 
@@ -134,6 +89,7 @@ resultsDT <- data.table(as.matrix(predicted_output))
 resultsDT[, predict:=NULL]
 
 source("mcLogLoss.R")
+valDT <- data.table( as.matrix(train_hex_split[[2]]) )
 mcLogLoss(valDT$output, resultsDT)
 
 
