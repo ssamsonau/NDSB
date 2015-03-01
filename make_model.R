7a7919c8d77d80f33d40a51d1ae28311ed8f1338
ssamsonau <ssamsonau@gc.cuny.edu>
2015-03-01 15:19:50 -0500

    41, ballance classes

--- make_model.R
+++ make_model.R
@@ -4,34 +4,35 @@ t1 <- Sys.time()
 library(data.table)
 imgTrainDT <- fread("41features.csv")
 setnames(imgTrainDT, 1, "path")
+
 pathCol <- imgTrainDT$path
 imgTrainDT[, path:=NULL]
 
-#library(EBImage)
-#display(matrix(imgTrainDT[5000, .SD, .SDcols=1:900], ncol=30))
-
 #preprocess with caret
-#--------------------------------------------------
 library(caret)
-#nzv <- nearZeroVar(imgTrainDT)
-#imgTrainDT[, eval(nzv):=NULL]
+c_s_trans <- preProcess(imgTrainDT, method  = c("center", "scale"))
+imgTrainDT <- data.table( predict(c_s_trans, imgTrainDT) )
 
-#descrCor <- cor(imgTrainDT)
-#highlyCorDescr <- findCorrelation(descrCor, cutoff = .9)
-#imgTrainDT[, eval(highlyCorDescr):=NULL]
-
-#pca_trans <- preProcess(imgTrainDT, method  = c("center", "scale", "pca"), thresh=0.99)
-#imgTrainDT <- data.table( predict(pca_trans, imgTrainDT) )
-#--------------------------------------------------
-
-imgTrainDT[, .outcome:=
-             sapply( strsplit( pathCol, "&"), "[", 1) ]
-
+imgTrainDT[, .outcome:= sapply( strsplit( pathCol, "&"), "[", 1) ]
 imgTrainDT[grep("shrimp-like_other", .outcome), .outcome:="shrimp_like_other"]
+
+#imgTrainDT_kn <- imgTrainDT[ grep("unknown", pathCol, invert = T), ]
+#imgTrainDT_un <- imgTrainDT[ grep("unknown", pathCol), ]
+
+
+source("balance_classes.R")
+hist(imgTrainDT[, .N, by=.outcome]$N)
+imgTrainDT <- balance_classes(min.size = 500, max.size = 1000, imgTrainDT = imgTrainDT)
+
+
+#imgTrainDT_kn[, .outcome:=factor(.outcome)]
+#imgTrainDT_un[, .outcome:=factor(.outcome)]
+
 imgTrainDT[, .outcome:=factor(.outcome)]
 
 ##-------------------------------------------------------
 #"train a model for outcome"
+library(caret)
 set.seed(3456)
 
 source("mcLogLoss_metrics.R")
@@ -48,17 +49,19 @@ library(doParallel);  cl <- makeCluster(detectCores());  registerDoParallel(cl)
 #parallel in Unix
 #require('doMC');  registerDoMC()
 
-#Grid <-  expand.grid(mtry = c(2, 4, 8, 16, 32, 64, 128) )
-#Grid <- expand.grid(C=c(1e-5, 1e-3, 1e-1, 10, 100))
+#Grid <-  expand.grid(mtry=c(2, 21, 41),  maxnodes=c(5, 10, 20) )
+#Grid <- expand.grid(C=c(1e-1, 1, 10, 100, 1000))
+#Grid <- expand.grid(C=c(1e-2, 1e-1, 1, 10, 100))
 
 #imgTrainDT[, cl:=as.numeric(imgTrainDT$.outcome)]
-#Fit <- train(factor(.outcome) ~ ., data = imgTrainDT[1000:1200],
+#Fit <- train(factor(.outcome) ~ ., data = imgTrainDT_kn[.outcome %in% 
+#                                                          unique(imgTrainDT_kn$.outcome)[1:20]],
 Fit <- train(.outcome ~ ., data = imgTrainDT,       
                method = "rf",
                
-               ntree=500, 
-               trControl = fitControl, 
-               metric="Kappa" 
+               ntree=50,
+               trControl = fitControl 
+               ,metric="Kappa" 
                #,tuneGrid=Grid
                )
 
@@ -66,7 +69,7 @@ print(Fit)
 #stopCluster(cl)
 #system2("C://Windows/System32/cmd.exe", "taskkill /F /IM Rscript.exe")
 
-save(Fit, file="model_rf_41features.Rdata")
+save(Fit, file="model_rf_41features_Ball.Rdata")
 
 t2 <- Sys.time()
 print(t2-t1)
