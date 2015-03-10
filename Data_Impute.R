148a7d0e92af5f06f5bcd7c034df40126a929d75
ssamsonau <ssamsonau@gc.cuny.edu>
2015-03-10 16:45:42 -0400

    impute data in separate file

---
+++ Data_Impute.R
@@ -0,0 +1,39 @@
+#load train And test data
+library(data.table)
+
+imgTrainDT <- fread("316features.csv")
+setnames(imgTrainDT, 1, "path")
+pathCol <- imgTrainDT$path
+imgTrainDT[, path:=NULL]
+
+library(caret)
+nzv <- nearZeroVar(imgTrainDT)
+imgTrainDT[, eval(nzv):=NULL]
+
+###infinite to NA and the impute
+imgTrainDT[, names(imgTrainDT):=lapply(.SD, function(x){replace(x, is.infinite(x), NA)}), 
+           .SDcols=1:ncol(imgTrainDT)]
+knn_imp <-  preProcess(imgTrainDT, method = "knnImpute", k=5)
+imgTrainDT <- data.table(predict(knn_imp, imgTrainDT))
+
+imgTrainDT[, .output:=sapply(strsplit(pathCol, "&"), "[", 1)]
+
+write.csv(imgTrainDT, file=paste0(316, "features_imp.csv")  )
+
+
+################   test
+imgTestDT <- fread("316featuresTest.csv")
+setnames(imgTestDT, 1, "filename")
+fileNameCol <- imgTestDT$filename
+imgTestDT[, filename:=NULL]
+
+#nzv <- nearZeroVar(imgTrainDT)
+imgTestDT[, eval(nzv):=NULL]
+
+imgTestDT[, names(imgTestDT):=lapply(.SD, function(x){replace(x, is.infinite(x), NA)}), 
+          .SDcols=1:ncol(imgTestDT)]
+knn_imp <-  preProcess(imgTestDT, method = "knnImpute", k=5)
+imgTestDT <- data.table(predict(knn_imp, imgTestDT))
+
+imgTestDT[, filename:=fileNameCol]
+write.csv(imgTrainDT, file=paste0(316, "featuresTest_imp.csv")  )
